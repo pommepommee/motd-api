6f02a34da3a6b088b4d447913d22639fe923687b
pommepommee <thomasgpro@hotmail.com>
2022-02-02 10:58:05 +0100

    :hammer:

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,8 @@
 
 # Docker Hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME"
-docker build -t 
+docker build -t tgigout/motd-api
+docker push tgigout/motd-api
+
+# Gcloud
+kubectl apply -f deploy.yaml
